53d6814d321b1ebf2b37aa4d926f46debf0d5a4e
dark-end5 <christtheanchormentors@gmail.com>
2026-06-21 05:24:39 +0300

    Refactor bot.js for configuration and exports

--- bot.js
+++ bot.js
@@ -1,6 +1,4 @@
-// Bot configuration and initialization
-// Import your modules and initialize your bot here
-
-module.exports = {
-  // Add your bot exports here
-};
+{
+  "name": "PairBot",
+  "prefix": "!"
+}
